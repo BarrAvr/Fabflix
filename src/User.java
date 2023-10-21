f12941cff5fb12247f18d1f000a45e398009711e
Mingjia Wang <mingjw2@uci.edu>
2023-10-21 13:31:44 -0700

    Added void return type label to addToCart method in User.java

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This User class only has the username field in this example.
  * You can add more attributes such as the user's shopping cart items.
@@ -5,9 +7,26 @@
 public class User {
 
     private final String username;
+    private ArrayList<String> cart;
 
     public User(String username) {
         this.username = username;
+        this.cart = new ArrayList<>();
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String setUsername() {
+        return username;
+    }
+
+    public ArrayList<String> getCart() {
+        return cart;
+    }
+
+    public void addToCart(String item) {
+        cart.add(item);
+    }
 }
