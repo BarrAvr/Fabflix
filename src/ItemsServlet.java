5bf6259bfb6e3ee2a1cc935b23c1b28f2165f627
Mingjia Wang <mingjw2@uci.edu>
2023-10-27 14:26:31 -0700

    Completely finished ItemsServlet -- got doGet and doPost working

--- src/ItemsServlet.java
+++ src/ItemsServlet.java
@@ -9,62 +9,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-// Declaring a WebServlet called ItemServlet, which maps to url "/items"
 @WebServlet(name = "ItemServlet", urlPatterns = "/items")
 
 public class ItemsServlet extends HttpServlet {
         public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         HttpSession session = request.getSession();
-
         ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems");
-//
-//        // If "previousItems" is not found on session, means this is a new user, thus we create a new previousItems
-//        // ArrayList for the user
-//        if (previousItems == null) {
-//
-//            // Add the newly created ArrayList to session, so that it could be retrieved next time
-//            previousItems = new ArrayList<String>();
-//            session.setAttribute("previousItems", previousItems);
-//        }
-//
-//        // Log to localhost log
-//        request.getServletContext().log("getting " + previousItems.size() + " items");
-//
-//        String newItem = request.getParameter("newItem"); // Get parameter that sent by GET request url
-//
-//        response.setContentType("text/html");
-//        PrintWriter out = response.getWriter();
-//        String title = "Items Purchased";
-//
-//        out.println(String.format("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n" +
-//                "<html>\n" +
-//                "   <head>" +
-//                "   <title>%s</title>" +
-//                "   </head>\n" +
-//                "   <body bgcolor=\"#FDF5E6\">\n" +
-//                "       <h1>%s</h1>", title, title));
-//
-//        // In order to prevent multiple clients, requests from altering previousItems ArrayList at the same time, we
-//        // lock the ArrayList while updating
-//        synchronized (previousItems) {
-//            if (newItem != null) {
-//                previousItems.add(newItem); // Add the new item to the previousItems ArrayList
-//            }
-//
-//            // Display the current previousItems ArrayList
-//            if (previousItems.size() == 0) {
-//                out.println("<i>No items</i>");
-//            } else {
-//                out.println("<ul>");
-//                for (String previousItem : previousItems) {
-//                    out.println("<li>" + previousItem);
-//                }
-//                out.println("</ul>");
-//            }
-//        }
-//
-//        out.println("</body></html>");
+        if (previousItems == null) {
+            previousItems = new ArrayList<String>();
+        }
+
+        PrintWriter out = response.getWriter();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("cart", previousItems.toString());
+        out.write(jsonObject.toString());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -75,18 +34,16 @@ public class ItemsServlet extends HttpServlet {
             JsonObject result = new JsonObject();
 
             String newItem = request.getParameter("newItem");
-            // Get a instance of current session on the request
+
             HttpSession session = request.getSession();
 
-//      // Retrieve data named "previousItems" from session
             ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems");
             System.out.println(previousItems);
 
             if (previousItems == null) {
                 previousItems = new ArrayList<String>();
             }
-//      // If "previousItems" is not found on session, means this is a new user, thus we create a new previousItems
-            // ArrayList for the user
+
             synchronized (previousItems) {
 
                 previousItems.add(newItem);
