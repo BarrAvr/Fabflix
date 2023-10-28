a21bd1a6c3e71408a36c517e90dfacb722f0b356
Mingjia Wang <mingjw2@uci.edu>
2023-10-28 11:03:06 -0700

    Got shopping cart working at Starbucks on Sat morning, but only if cart is empty -- POST request to add new items doesn't work yet

--- WebContent/shopping-cart.js
+++ WebContent/shopping-cart.js
@@ -8,11 +8,13 @@ function handleShoppingCartData(resultData) {
     console.log(cartArray);
     console.log(typeof cartArray);
     if (cartArray.length == 0) {
-        shoppingCartElement.innerHTML = "<p>No items in cart</p>";
+        shoppingCartElement.append("<li>No items in cart</li>");
+    } else {
+        cartArray.forEach((item) => {
+            console.log("appending item " + item + " to cart as li");
+            shoppingCartElement.append("<li>" + item + "</li>");
+        })
     }
-    cartArray.forEach((item) => {
-        shoppingCartElement.append(`<li>${item}</li>`)
-    });
 }
 
 // Makes the HTTP GET request and registers on success callback function handleStarResult
