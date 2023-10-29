7183fcd953910b56ae3f52b513aa18af1e396b11
Mingjia Wang <mingjw2@uci.edu>
2023-10-29 12:43:22 -0700

    Got jump functionality working after fixing inception bug in single-movie.html

--- WebContent/jumpFunctionality.js
+++ WebContent/jumpFunctionality.js
@@ -1,15 +1,54 @@
-jQuery.ajax({
-    dataType: "json",
-    method: "GET",
-    url: "list-state",
-    success: function (listResult) {
-        console.log(listResult.type)
-        jQuery.ajax({
-            dataType: "json",
-            method: "GET",
-            url: "api/search-results",
-            data : listResult,
-            success: (resultData) => handleSearchResult(resultData)
-        });
-    }
-});
+function handleJumpBack(listState) {
+    let url = "search-results.html?";
+
+       if (listState["type"] === "genre") {
+           url += "type=genre&";
+           url += "genre=" + listState["genre"];
+           url += "&";
+       } else if (listState["type"] === "prefix") {
+           url += "type=prefix&";
+           url += "prefix=" + listState["prefix"];
+           url += "&";
+       } else {
+           url += "type=general&";
+           url += "title=";
+           if (listState["title"] != null) {
+               url += listState["title"];
+           }
+           url += "&";
+           url += "star=";
+           if (listState["star"] != null) {
+               url += listState["star"];
+           }
+           url += "&";
+           url += "year=";
+           if (listState["year"] != null) {
+               url += listState["year"];
+           }
+           url += "&";
+           url += "director=";
+           if (listState["director"] != null) {
+               url += listState["director"];
+           }
+           url += "&";
+
+       }
+       url += "sortBy=" + listState["sortBy"];
+       url += "&";
+
+       url += "titleOrder=" + listState["titleOrder"];
+       url += "&";
+
+       url += "ratingOrder=" + listState["ratingOrder"];
+
+    window.location.replace(url);
+}
+
+function jumpback() {
+    jQuery.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "list-state",
+        success: (resultData) => handleJumpBack(resultData)
+    });
+}
