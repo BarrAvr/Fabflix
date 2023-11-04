d4df84a906f8dfff42aeceef592625cf754946a4
Mingjia Wang <mingjw2@uci.edu>
2023-11-04 12:35:07 -0700

    Confirmed Recaptcha still works after first round of reformatting; more cleaning up is still needed

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -45,24 +45,19 @@ public class LoginServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
 
-        try {
-            RecaptchaVerifyUtils.verify(gRecaptchaResponse);
-            JsonObject responseJsonObject = new JsonObject();
-            responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
-            out.write(responseJsonObject.toString());
-            System.out.println("success - Writing " + responseJsonObject.toString() + " to out");
-            out.close();
-        } catch (Exception e) {
-            JsonObject responseJsonObject = new JsonObject();
-            System.out.println("Entered catch block due to Recaptcha errror: " + e.getMessage());
-            responseJsonObject.addProperty("status", "fail");
-            responseJsonObject.addProperty("message", "Login failed - Recaptcha verification error: " + e.getMessage());
-            out.write(responseJsonObject.toString());
+//        try {
 
-            out.close();
-            return;
-        }
+//        }
+//        catch (Exception e) {
+//            JsonObject responseJsonObject = new JsonObject();
+//            System.out.println("Entered catch block due to Recaptcha errror: " + e.getMessage());
+//            responseJsonObject.addProperty("status", "fail");
+//            responseJsonObject.addProperty("message", "Login failed - Recaptcha verification error: " + e.getMessage());
+//            out.write(responseJsonObject.toString());
+//
+//            out.close();
+//            return;
+//        }
 
 
         request.getServletContext().log("getting username: " + userEnteredUsername);
@@ -71,6 +66,14 @@ public class LoginServlet extends HttpServlet {
         JsonArray jsonArray = new JsonArray();
 
         try {
+
+            RecaptchaVerifyUtils.verify(gRecaptchaResponse);
+            JsonObject responseJsonObject = new JsonObject();
+            responseJsonObject.addProperty("status", "success");
+            responseJsonObject.addProperty("message", "success");
+            out.write(responseJsonObject.toString());
+            System.out.println("success - Writing " + responseJsonObject.toString() + " to out");
+            out.close();
 
             Connection conn = dataSource.getConnection();
             // Declare our statement
@@ -118,7 +121,7 @@ public class LoginServlet extends HttpServlet {
 
 
             // Output stream to STDOUT
-            JsonObject responseJsonObject = new JsonObject();
+//            JsonObject responseJsonObject = new JsonObject();
 
             if (loginCase == 1) {
                 // Login success:
@@ -154,7 +157,7 @@ public class LoginServlet extends HttpServlet {
             response.setStatus(200);
 
             conn.close();
-
+            
         } catch (Exception e) {
             // Write error message JSON object to output
             JsonObject responseJsonObject = new JsonObject();
