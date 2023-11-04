c1d4269bc77a161242c280284d129112cb6053c6
Mingjia Wang <mingjw2@uci.edu>
2023-11-03 20:30:45 -0700

    Got LoginServlet to block login when Recaptcha box isn't checked

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -34,6 +34,8 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
 
+        PrintWriter out = response.getWriter();
+
         String userEnteredUsername = request.getParameter("username");
         String userEnteredPassword = request.getParameter("password");
         String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
@@ -43,110 +45,38 @@ public class LoginServlet extends HttpServlet {
         System.out.println("password: " + userEnteredPassword);
         System.out.println("gRecaptchaResponse: " + gRecaptchaResponse);
 
-        request.getServletContext().log("getting username: " + userEnteredUsername);
-
-        PrintWriter out = response.getWriter();
-        JsonArray jsonArray = new JsonArray();
-
+        // Verify reCAPTCHA
         try {
-
-            Connection conn = dataSource.getConnection();
-            // Declare our statement
-            Statement statement = conn.createStatement();
-
-            String query = String.format("select email, password, id from customers where email = \"%s\"", userEnteredUsername);
-//            String query = "select * from movies as m, ratings as r, stars_in_movies as sim, stars as s, genres_in_movies as gim, genres as g " +
-//                    "where m.id = r.movieId and r.movieId = sim.movieId and sim.starId = s.id and sim.starId = s.id and gim.movieId = m.id and gim.genreId = g.id order by r.rating desc";
-
-            // Perform the query
-            ResultSet rs = statement.executeQuery(query);
-            // The log message can be found in localhost log
-            request.getServletContext().log("getting username: " + userEnteredUsername);
-
-//            cases:
-//            1 - success; 2 - username matched but password didn't; 3 - user not found / nothing matched
-            int loginCase = 0;
-
-            int size = 0;
-            String email = "", pass = "", id = "";
-            while (rs.next()) {
-                size++;
-                email = rs.getString("email");
-                pass = rs.getString("password");
-                id = rs.getString("id");
-            }
-
-            if (size > 0) {
-                System.out.println("Found " + email + " " + pass);
-                boolean usernameMatch = email.equals(userEnteredUsername);
-                boolean passwordMatch = pass.equals(userEnteredPassword);
-                System.out.println("Comparison: " + usernameMatch + " " + passwordMatch);
-                if (usernameMatch && passwordMatch) {
-                    loginCase = 1;
-                } else if (usernameMatch && !passwordMatch) {
-                    loginCase = 2;
-                }
-            } else {
-                System.out.println("Did not find username in DB");
-                loginCase = 3;
-            }
-
-
-
-
-
-            // Output stream to STDOUT
+            RecaptchaVerifyUtils.verify(gRecaptchaResponse);
             JsonObject responseJsonObject = new JsonObject();
 
-            if (loginCase == 1) {
-                // Login success:
-
-
-                // set this user into the session
-                request.getSession().setAttribute("user", new User(userEnteredUsername));
-                request.getSession().setAttribute("id", id);
-                request.getSession().setAttribute("email", email);
-
-                responseJsonObject.addProperty("status", "success");
-                responseJsonObject.addProperty("message", "success");
-//                responseJsonObject.addProperty("email, id", email + " " + id);
-
-            } else {
-                // Login fail
-                responseJsonObject.addProperty("status", "fail");
-                // Log to localhost log
-                request.getServletContext().log("Login failed");
-                // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-                if (loginCase == 3) {
-//                    responseJsonObject.addProperty("message", "user " + userEnteredUsername + " does not exist");
-                    responseJsonObject.addProperty("message", "Login failed: either your username or password was incorrect");
-                } else {
-//                    responseJsonObject.addProperty("message", "user " + userEnteredUsername + " found, but password incorrect");
-                    responseJsonObject.addProperty("message", "Login failed: either your username or password was incorrect");
-                }
-            }
-            String jsonOutput = responseJsonObject.toString();
-            System.out.println("In try block - Writing " + jsonOutput + " to out");
-            response.getWriter().write(responseJsonObject.toString());
-
-            response.setStatus(200);
-
-            conn.close();
-
+            responseJsonObject.addProperty("status", "success");
+            responseJsonObject.addProperty("message", "success");
+            out.write(responseJsonObject.toString());
         } catch (Exception e) {
-            // Write error message JSON object to output
             JsonObject responseJsonObject = new JsonObject();
             responseJsonObject.addProperty("errorMessage", e.getMessage());
             String jsonOutput = responseJsonObject.toString();
             System.out.println("In catch block - Writing " + jsonOutput + " to out");
             response.getWriter().write(responseJsonObject.toString());
+            out.close();
+            return;
+        }
 
-            // Set response status to 500 (Internal Server Error)
-            response.setStatus(500);
+
+//        } catch (Exception e) {
+//            // Write error message JSON object to output
+//            JsonObject responseJsonObject = new JsonObject();
+//            responseJsonObject.addProperty("errorMessage", e.getMessage());
+//            String jsonOutput = responseJsonObject.toString();
+//            System.out.println("In catch block - Writing " + jsonOutput + " to out");
+//            response.getWriter().write(responseJsonObject.toString());
+//
+//            // Set response status to 500 (Internal Server Error)
+//            response.setStatus(500);
 //        } finally {
 //            out.close();
 //        }
-
         }
     }
-}
+
