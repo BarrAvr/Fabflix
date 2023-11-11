33bf4df2ce20eb7ba3e8bf2c6ccbd3d659afe3eb
Mingjia Wang <mingjw2@uci.edu>
2023-11-10 21:13:56 -0800

    Got employee login auth working with an actual PreparedStatement database query (next up: password encryption)

--- src/EmployeeLoginServlet.java
+++ src/EmployeeLoginServlet.java
@@ -13,6 +13,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -41,74 +42,62 @@ public class EmployeeLoginServlet extends HttpServlet {
         String userEnteredPassword = request.getParameter("password");
 
         try {
+            Connection conn = dataSource.getConnection();
+            String employeeQuery = "select email, password from employees where email = ?";
+            PreparedStatement stat = conn.prepareStatement(employeeQuery);
+            stat.setString(1, userEnteredUsername);
+            ResultSet rs = stat.executeQuery();
 
-//            Connection conn = dataSource.getConnection();
-//            // Declare our statement
-//            Statement statement = conn.createStatement();
 //
-//            String query = String.format("select email, password, id from customers where email = \"%s\"", userEnteredUsername);
-////            String query = "select * from movies as m, ratings as r, stars_in_movies as sim, stars as s, genres_in_movies as gim, genres as g " +
-////                    "where m.id = r.movieId and r.movieId = sim.movieId and sim.starId = s.id and sim.starId = s.id and gim.movieId = m.id and gim.genreId = g.id order by r.rating desc";
 //
-//            // Perform the query
-//            ResultSet rs = statement.executeQuery(query);
-//            // The log message can be found in localhost log
-//            request.getServletContext().log("getting username: " + userEnteredUsername);
-//
-////            cases:
-////            1 - success; 2 - username matched but password didn't; 3 - user not found / nothing matched
-//            int loginCase = 0;
-//
-//            int size = 0;
-//            String email = "", pass = "", id = "";
-//            while (rs.next()) {
-//                size++;
-//                email = rs.getString("email");
-//                pass = rs.getString("password");
-//                id = rs.getString("id");
-//            }
-//
-//            if (size > 0) {
-//                System.out.println("Found " + email + " " + pass);
-//                boolean usernameMatch = email.equals(userEnteredUsername);
-//                boolean passwordMatch = pass.equals(userEnteredPassword);
-//                passwordMatch = VerifyPassword.verifyCredentials(userEnteredUsername, userEnteredPassword);
-//                System.out.println("Comparison: " + usernameMatch + " " + passwordMatch);
-//                if (usernameMatch && passwordMatch) {
-//                    loginCase = 1;
-//                } else if (usernameMatch && !passwordMatch) {
-//                    loginCase = 2;
-//                }
-//            } else {
-//                System.out.println("Did not find username in DB");
-//                loginCase = 3;
-//            }
+            int size = 0;
+            String email = "", pass = "";
+            while (rs.next()) {
+                size++;
+                email = rs.getString("email");
+                pass = rs.getString("password");
+            }
 
-            boolean employeeUsernameMatch = userEnteredUsername.equals("classta@email.edu");
-            boolean employeePasswordMatch = userEnteredPassword.equals("classta");
+            if (size > 0) {
+                System.out.println("Found " + email + " " + pass);
+                boolean employeeUsernameMatch = userEnteredUsername.equals(email);
+                boolean employeePasswordMatch = userEnteredPassword.equals(pass);
+                System.out.println("Comparison: " + employeeUsernameMatch + " " + employeePasswordMatch);
 
-
-            if (employeeUsernameMatch && employeePasswordMatch) {
-
-                request.getSession().setAttribute("employee", userEnteredUsername);
-                System.out.println("EmployeeLoginServlet hit, setting employee in session to: " + request.getSession().getAttribute("employee"));
-
-                System.out.println("username: " + userEnteredUsername);
-                System.out.println("password: " + userEnteredPassword);
-
-                JsonObject responseJsonObject = new JsonObject();
-                responseJsonObject.addProperty("status", "success");
-                response.getWriter().write(responseJsonObject.toString());
+                if (employeeUsernameMatch && employeePasswordMatch) {
+                    request.getSession().setAttribute("employee", userEnteredUsername);
+                    System.out.println("Employee logged in successfully, setting employee in session to: " + request.getSession().getAttribute("employee"));
+                    JsonObject responseJsonObject = new JsonObject();
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+                    response.getWriter().write(responseJsonObject.toString());
+                } else {
+                    JsonObject responseJsonObject = new JsonObject();
+                    responseJsonObject.addProperty("status", "fail");
+                    responseJsonObject.addProperty("message", "There was an error logging in, please try again.");
+                    response.getWriter().write(responseJsonObject.toString());
+                }
             } else {
                 JsonObject responseJsonObject = new JsonObject();
                 responseJsonObject.addProperty("status", "fail");
-                responseJsonObject.addProperty("message", "There was an error logging in with the provided credetials. Please try again.");
+                responseJsonObject.addProperty("message", "There was an error logging in, please try again.");
                 response.getWriter().write(responseJsonObject.toString());
             }
+
             response.setStatus(200);
+//            
 
         } catch (Exception e) {
+            e.printStackTrace();
+            JsonObject responseJsonObject = new JsonObject();
+            responseJsonObject.addProperty("status", "fail");
+            responseJsonObject.addProperty("message", e.getMessage());
+            String jsonOutput = responseJsonObject.toString();
+            System.out.println("In catch block - Writing " + jsonOutput + " to out");
+            response.getWriter().write(responseJsonObject.toString());
 
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(200);
         }
 
 
