78f43675654b874b13883f844072c226e4e8c207
Mingjia Wang <mingjw2@uci.edu>
2023-11-11 21:00:56 -0800

    Renamed add-movie.html and add-movie.js to new-movie.html and new-movie.js to see if it will fix the AWS issue

---
+++ WebContent/_dashboard/new-movie.js
@@ -0,0 +1,71 @@
+let add_star_form = $("#add_movie_form");
+
+function submitNewStar() {
+
+}
+
+/**
+ * Handle the data returned by LoginServlet
+ * @param resultDataString jsonObject
+ */
+function handleNewMovieResult(resultDataString) {
+    console.log("Result Data String");
+    console.log(resultDataString);
+    // let resultDataJson = JSON.stringify(resultDataString);
+    // console.log("Result Data JSON");
+    // console.log(resultDataJson);
+
+    // console.log("handle login response");
+    // console.log(resultDataJson);
+    // console.log(resultDataJson["status"]);
+
+    // If login succeeds, it will redirect the user to index.html
+    if (resultDataString["status"] === "success") {
+        console.log(resultDataString);
+        $("#add_movie_message").text(resultDataString["message"]);
+    } else {
+        // If login fails, the web page will display
+        // error messages on <div> with id "login_error_message"
+        console.log("show error message");
+        console.log(resultDataString["message"]);
+        $("#add_movie_message").text(resultDataString["message"]);
+    }
+}
+
+/**
+ * Submit the form content with POST method
+ * @param formSubmitEvent
+ */
+function submitNewMovieForm(formSubmitEvent) {
+    console.log("submit new movie form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    formSubmitEvent.preventDefault();
+
+    const data = {
+        title: document.getElementById("title").value,
+        year: document.getElementById("year").value,
+        director: document.getElementById("director").value,
+        star: document.getElementById("star").value,
+        genre: document.getElementById("genre").value,
+    };
+
+    console.log("data");
+    console.log(data);
+
+    $.ajax(
+        "api/add-movie", {
+            method: "POST",
+            // Serialize the login form to the data sent by POST request
+            data: data,
+            success: handleNewMovieResult
+        }
+    );
+}
+
+// Bind the submit action of the form to a handler function
+add_star_form.submit(submitNewMovieForm);
+
