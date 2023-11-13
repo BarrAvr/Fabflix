c7d6176d234cf0c6be67d051e66f23f242faa923
Mingjia Wang <mingjw2@uci.edu>
2023-11-13 07:15:36 +0000

    Updated keys for recaptcha on AWS

--- src/RecaptchaConstants.java
+++ src/RecaptchaConstants.java
@@ -1,5 +1,5 @@
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6LcbrugoAAAAAH38InE-PCJlgBv_c72Bhnbo6IYB";
+    public static final String SECRET_KEY ="6LdiHg0pAAAAAFwD6q2Ytu3MZb9gWJOrGDXTscnI";
 
 }
