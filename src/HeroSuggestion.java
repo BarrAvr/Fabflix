7e1f1053a26e73d866690e707c4d140260168129
Mingjia Wang <mingjw2@uci.edu>
2023-11-19 16:31:09 -0800

    Got fulltext search query working in HeroSuggestion after commenting out all example code and making the select in boolean mode in the shell of the servlet & deleting the old war from webapps

--- src/HeroSuggestion.java
+++ src/HeroSuggestion.java
@@ -1,12 +1,19 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 
 @WebServlet("/autocomplete/hero-suggestion")
@@ -16,46 +23,57 @@ public class HeroSuggestion extends HttpServlet {
 	 * Key is hero ID. Value is hero name.
 	 */
 	public static HashMap<Integer, String> superHeroMap = new HashMap<>();
-	
-	static {
-		superHeroMap.put(1, "Blade");
-		superHeroMap.put(2, "Ghost Rider");
-		superHeroMap.put(3, "Luke Cage");
-		superHeroMap.put(4, "Silver Surfer");
-		superHeroMap.put(5, "Beast");
-		superHeroMap.put(6, "Thing");
-		superHeroMap.put(7, "Black Panther");
-		superHeroMap.put(8, "Invisible Woman");
-		superHeroMap.put(9, "Nick Fury");
-		superHeroMap.put(10, "Storm");
-		superHeroMap.put(11, "Iron Man");
-		superHeroMap.put(12, "Professor X");
-		superHeroMap.put(13, "Hulk");
-		superHeroMap.put(14, "Cyclops");
-		superHeroMap.put(15, "Thor");
-		superHeroMap.put(16, "Jean Grey");
-		superHeroMap.put(17, "Wolverine");
-		superHeroMap.put(18, "Daredevil");
-		superHeroMap.put(19, "Captain America");
-		superHeroMap.put(20, "Spider-Man");
-		superHeroMap.put(101, "Superman");
-		superHeroMap.put(102, "Batman");
-		superHeroMap.put(103, "Wonder Woman");
-		superHeroMap.put(104, "Flash");
-		superHeroMap.put(105, "Green Lantern");
-		superHeroMap.put(106, "Catwoman");
-		superHeroMap.put(107, "Nightwing");
-		superHeroMap.put(108, "Captain Marvel");
-		superHeroMap.put(109, "Aquaman");
-		superHeroMap.put(110, "Green Arrow");
-		superHeroMap.put(111, "Martian Manhunter");
-		superHeroMap.put(112, "Batgirl");
-		superHeroMap.put(113, "Supergirl");
-		superHeroMap.put(114, "Black Canary");
-		superHeroMap.put(115, "Hawkgirl");
-		superHeroMap.put(116, "Cyborg");
-		superHeroMap.put(117, "Robin");
+
+	private DataSource dataSource;
+
+	public void init(ServletConfig config) {
+		System.out.println("init");
+		try {
+			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
+
+//	static {
+//		superHeroMap.put(1, "Blade");
+//		superHeroMap.put(2, "Ghost Rider");
+//		superHeroMap.put(3, "Luke Cage");
+//		superHeroMap.put(4, "Silver Surfer");
+//		superHeroMap.put(5, "Beast");
+//		superHeroMap.put(6, "Thing");
+//		superHeroMap.put(7, "Black Panther");
+//		superHeroMap.put(8, "Invisible Woman");
+//		superHeroMap.put(9, "Nick Fury");
+//		superHeroMap.put(10, "Storm");
+//		superHeroMap.put(11, "Iron Man");
+//		superHeroMap.put(12, "Professor X");
+//		superHeroMap.put(13, "Hulk");
+//		superHeroMap.put(14, "Cyclops");
+//		superHeroMap.put(15, "Thor");
+//		superHeroMap.put(16, "Jean Grey");
+//		superHeroMap.put(17, "Wolverine");
+//		superHeroMap.put(18, "Daredevil");
+//		superHeroMap.put(19, "Captain America");
+//		superHeroMap.put(20, "Spider-Man");
+//		superHeroMap.put(101, "Superman");
+//		superHeroMap.put(102, "Batman");
+//		superHeroMap.put(103, "Wonder Woman");
+//		superHeroMap.put(104, "Flash");
+//		superHeroMap.put(105, "Green Lantern");
+//		superHeroMap.put(106, "Catwoman");
+//		superHeroMap.put(107, "Nightwing");
+//		superHeroMap.put(108, "Captain Marvel");
+//		superHeroMap.put(109, "Aquaman");
+//		superHeroMap.put(110, "Green Arrow");
+//		superHeroMap.put(111, "Martian Manhunter");
+//		superHeroMap.put(112, "Batgirl");
+//		superHeroMap.put(113, "Supergirl");
+//		superHeroMap.put(114, "Black Canary");
+//		superHeroMap.put(115, "Hawkgirl");
+//		superHeroMap.put(116, "Cyborg");
+//		superHeroMap.put(117, "Robin");
+//	}
 
     /*
      * 
@@ -78,29 +96,70 @@ public class HeroSuggestion extends HttpServlet {
      * 
      */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet hit in HeroSuggestion");
+
+		response.setContentType("application/json");
+
 		try {
 			// setup the response json arrray
 			JsonArray jsonArray = new JsonArray();
-			
+
+			Connection conn = dataSource.getConnection();
+
+			String query = "select * from movies where match(title) against('good* u*' in boolean mode)";
+
+			System.out.println("Executing query " + query);
+			PreparedStatement statement = conn.prepareStatement(query);
+
+			ResultSet rs = statement.executeQuery();
+			System.out.println("query executed in HeroSuggestion");
+			int row = 1;
+
+			while (rs.next()) {
+				System.out.println("Parsing through movie" + row + " in HeroSuggestion");
+
+				String id = rs.getString("id");
+				String title = rs.getString("title");
+				String year = rs.getString("year");
+				String director = rs.getString("director");
+
+				// Create a JsonObject based on the data we retrieve from rs
+
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("id", id);
+				jsonObject.addProperty("title", title);
+				jsonObject.addProperty("year", year);
+				jsonObject.addProperty("director", director);
+				System.out.println("Found movie: " + jsonObject);
+
+				jsonArray.add(jsonObject);
+				row++;
+			}
+			rs.close();
+			statement.close();
+			conn.close();
+
 			// get the query string from parameter
-			String query = request.getParameter("query");
+//			String query = request.getParameter("query");
 			
 			// return the empty json array if query is null or empty
-			if (query == null || query.trim().isEmpty()) {
-				response.getWriter().write(jsonArray.toString());
-				return;
-			}	
+//			if (query == null || query.trim().isEmpty()) {
+//				response.getWriter().write(jsonArray.toString());
+//				return;
+//			}
 			
 			// search on superheroes and add the results to JSON Array
 			// this example only does a substring match
 			// TODO: in project 4, you should do full text search with MySQL to find the matches on movies and stars
-			
-			for (Integer id : superHeroMap.keySet()) {
-				String heroName = superHeroMap.get(id);
-				if (heroName.toLowerCase().contains(query.toLowerCase())) {
-					jsonArray.add(generateJsonObject(id, heroName));
-				}
-			}
+
+
+
+//			for (Integer id : superHeroMap.keySet()) {
+//				String heroName = superHeroMap.get(id);
+//				if (heroName.toLowerCase().contains(query.toLowerCase())) {
+//					jsonArray.add(generateJsonObject(id, heroName));
+//				}
+//			}
 			response.getWriter().write(jsonArray.toString());
 		} catch (Exception e) {
 			System.out.println(e);
