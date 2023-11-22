bf002e5b76d183d853c5d217b6127a6945fbd68d
Mingjia Wang <57849290+mingjia-wang@users.noreply.github.com>
2023-11-21 22:29:51 -0800

    Manually updating secret key in RecaptchaConstants.java because the changes I made on AWS got pushed to main by accident

--- src/RecaptchaConstants.java
+++ src/RecaptchaConstants.java
@@ -1,5 +1,5 @@
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6Le_MQ4pAAAAANY4CvFSqqBcFJR5G9XyMLKaGKoZ";
+    public static final String SECRET_KEY ="6LeHshYpAAAAAAOrKPKwWdiGnR_kGVWYFuztMNpM";
 
 }
