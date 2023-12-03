73468ad7331a88a2898fd542836b2a7962a8d9d5
mingjia-wang <mingjw2@uci.edu>
2023-12-03 19:06:57 +0000

    Updated Recaptcha constants for new 'original' instance

--- src/RecaptchaConstants.java
+++ src/RecaptchaConstants.java
@@ -1,4 +1,4 @@
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6LdL7xspAAAAAPHzJfRCg1nu5VVK5c2U-002Mf8B";
+    public static final String SECRET_KEY ="6LeheCQpAAAAABngzxQAvXmvOXM7j31E3MsAQa8Q";
 }
